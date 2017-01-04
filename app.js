413a7954d5d1824e4aa3ade437baa6aeb226c95c
JoshAnnis <Josh_Annis@yahoo.com>
2017-01-04 15:11:26 -0600

    modified apps.js and index.html

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
+function initSearch(people){
+	alert("welcome to Most Wanted.follow the prompts to find who your looking for.");
+	do{
+		var searchType = prompt("Do you know the name of who you seek if yes enter 'name' if not search by 'attributes'.")		
+	}while (!(searchType =="name" || searchType =="attributes"));
+	switch(searchType){
+		case"name"
+		var Person = getPerson(prompt("Enter person's first name"), prompt("Enter person's last name."), people);
+		mainMenu(person,people);
+		break;
+		case "attributes"
+		var Height = prompt("How tall is the individual that you seek");
 
-/*  Remove this from your final submission
-function printAllToConsole(dataObj){
-	for (var key in dataObj) {
-		if (dataObj.hasOwnProperty(key)) {
-			console.log(key + " -> " + JSON.stringify(dataObj[key]));
-		}
+		var weight = prompt("How much does the individual weigh");
+
+		var Haircolor = prompt("What color hair do the individual have");
+
+		var Eyecolor = prompt("What color eyes does the individual have");
+
+		default:
+		alert("there was an error processing your request");
+		initMostWanted(people);
 	}
 }
-printAllToConsole(data);
-*/
 
-function initSearch(data){
-	alert("Hello World");
 
-	// get all the information you need to run the search
-	var yourName = prompt("Who do you want to search for?");
+		function getPerson(firstname, lastname, people){
 
-	// then pass that info to the respective function.
-	var result = getPersonInfo("J", "T")
 
-	// once the search is done, pass the results to the responder function
-	responder(result);
+
 }
 
-function responder(results){
-	// results may be a list of strings, an object, or a single string.
-	alert(results);
+		function mainMenu(person,people){
+		alert("could not find that person");
+		return initSearch(people);
+		}
+		var displayOption = prompt("found" + person.firstname + "" + person.lastname + "do you want to know their 'info' 'family , 'next of kin', or 'Descendants'? type the option you want or 'restart' , or 'quit' ")
+		
+
+	switch
+
 }
 
-function getPersonInfo(firstname, lastname){
-	var result = "This will be the information for whoever you searched for";
-	// return the object of a person
-	return result;
-}
 
-function getFamily(){
-	// return an array containing the members of the person's family
-	// the array should contain each person's unmodified object
-}
 
-// there will be much more here, and some of the code above will certainly change
+
+
