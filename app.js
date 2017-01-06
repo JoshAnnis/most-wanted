a04b2d65bf3792204d18a220743faf10b8daa6af
JoshAnnis <Josh_Annis@yahoo.com>
2017-01-06 07:08:33 -0600

    modified app.js and index.html

--- app.js
+++ app.js
@@ -1,57 +1,109 @@
 
 /*  Remove this from your final submission
 function printAllToConsole(dataObj){
-	for (var key in dataObj) {
-		if (dataObj.hasOwnProperty(key)) {
-			console.log(key + " -> " + JSON.stringify(dataObj[key]));
-		}
-	}
+    for (var key in dataObj) {
+        if (dataObj.hasOwnProperty(key)) {
+            console.log(key + " -> " + JSON.stringify(dataObj[key]));
+        }
+    }
 }
 printAllToConsole(data);
 */
 
-function initSearch(people){
-	alert("welcome to Most Wanted.follow the prompts to find who your looking for.");
-	do{
-		var searchType = prompt("Do you know the name of who you seek if yes enter 'name' if not search by 'attributes'.")		
-	}while (!(searchType =="name" || searchType =="attributes"));
-	switch(searchType){
-		case"name"
-		var person = getPerson(prompt("Enter person's first name"), prompt("Enter person's last name."), people);
-		mainMenu(person,people);
-		break;
-		case "attributes"
-		var Height = prompt("How tall is the individual that you seek");
+function initMostwanted(people) {
+    alert("welcome to Most Wanted. Follow the prompts to find who your looking for.");
+    do {
+        var searchType = prompt("Do you know the name of who you seek if yes enter 'name' if not search by 'attributes'.")
+    } while (!(searchType == "name" || searchType == "attributes"));
+    switch (searchType) {
+        case "name":
+            var listOfPeople = getPerson(prompt("Enter person's first name"), prompt("Enter person's last name."), people);
+            if (listOfPeople.length > 0)
+            mainMenu(listOfPeople[0], people);
+            break;
+        case "attributes":
+            var Height = prompt("How tall is the individual you seek?");
 
-		var weight = prompt("How much does the individual weigh");
+            var weight = prompt("How much does the individual weigh?");
 
-		var Haircolor = prompt("What color hair do the individual have");
+            var Haircolor = prompt("What color hair does the individual have?");
 
-		var Eyecolor = prompt("What color eyes does the individual have");
+            var Eyecolor = prompt("What color eyes does the individual have?");
 
-		default:
-		alert("there was an error processing your request");
-		initMostWanted(people);
-	}
+            var Gender = prompt("What gender is the individual?");
+        
+            var age = prompt("When's the individual's date of birth?"); 
+            mainMenu(people, person)             
+        break;
+    }
 }
 
 
-		function getPerson(firstname, lastname, people){
 
 
+function mainMenu (person, people){
+        if(!person)
+            alert("could not find who you are looking for");
+            initMostwanted(people);
+    
+
+    var getFamily = prompt("found"+ person.firstname +"" + person.lastname+ "", 'would you like to know about, family,  descendants,  next of kin, or would you like to exit or restart' );
+
+    switch (getFamily){
+            case "family":
+            var familyinfo = prompt()
+
+
+
+        break;
+
+
+            case "next of kin":
+
+
+
+
+        break;
+
+
+            case "descendants":
+
+
+
+        break;
+
+
+            case "restart":
+            initMostwanted(people)
+
+
+        break;
+
+
+            case "quit":
+
+
+         break;
+
+    }
 
 }
 
-		function mainMenu(person,people){
-		alert("could not find that person");
-		return initSearch(people);
-		}
-		var displayOption = prompt("found" + person.firstname + "" + person.lastname +"do you want to know their 'info' 'family , 'next of kin', or 'Descendants'? type the option you want or 'restart' , or 'quit' ")
-		
 
-	switch
+
+
+
+
+
+
+
+
+function getPerson(person, people) {
+     var listOfPeople = people.filter(person => person.firstname === fristname && person.lastname === lastname);
+     return listOfPeople;  
 
 }
+
 
 
 
