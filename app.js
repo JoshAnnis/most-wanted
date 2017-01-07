3e0df97b03ee5c9fcc0f38ed605f82a6182bd42c
JoshAnnis <Josh_Annis@yahoo.com>
2017-01-06 21:03:27 -0600

    fixed person.Name object

--- app.js
+++ app.js
@@ -11,29 +11,26 @@ printAllToConsole(data);
 */
 
 function initMostwanted(people) {
-    alert("welcome to Most Wanted. Follow the prompts to find who your looking for.");
+    alert("Welcome to Most Wanted. Follow the prompts to find who you're looking for.");
     do {
-        var searchType = prompt("Do you know the name of who you seek if yes enter 'name' if not search by 'attributes'.")
+        var searchType = prompt("Do you know the name of who you seek? If yes enter 'name' if not search by 'attributes'.")
     } while (!(searchType == "name" || searchType == "attributes"));
     switch (searchType) {
         case "name":
-            var listOfPeople = getPerson(prompt("Enter person's first name"), prompt("Enter person's last name."), people);
-            if (listOfPeople.length > 0)
-            mainMenu(listOfPeople[0], people);
-            break;
+        var person= getPerson(prompt("Enter person's first name."), prompt("Enter person's last name."), people);
+        mainMenu(person, people);
+        break;
         case "attributes":
-            var Height = prompt("How tall is the individual you seek?");
+        var Height = prompt("How tall is the individual?");
 
-            var weight = prompt("How much does the individual weigh?");
+        var weight = prompt("How much does the individual weigh?");
 
-            var Haircolor = prompt("What color hair does the individual have?");
+        var Eyecolor = prompt("What color eyes does the individual have?");
 
-            var Eyecolor = prompt("What color eyes does the individual have?");
+        var Gender = prompt("What gender is the individual?");
 
-            var Gender = prompt("What gender is the individual?");
-        
-            var age = prompt("When's the individual's date of birth?"); 
-            mainMenu(people, person)             
+        var age = prompt("When's the individual's date of birth?");
+        mainMenu(person, people);
         break;
     }
 }
@@ -41,71 +38,68 @@ function initMostwanted(people) {
 
 
 
-function mainMenu (person, people){
-        if(!person)
-            alert("could not find who you are looking for");
-            initMostwanted(people);
-    
-
-    var getFamily = prompt("found"+ person.firstname +"" + person.lastname+ "", 'would you like to know about, family,  descendants,  next of kin, or would you like to exit or restart' );
-
-    switch (getFamily){
-            case "family":
-            var familyinfo = prompt()
 
 
 
-        break;
 
+
+
+function mainMenu(person, people) {
+    if (!person){
+        alert("Could not find who you are looking for plase try again");
+        initMostwanted(people);
+    }
+        
+
+
+
+    var displayOption = prompt("Found " + person.firstName + person.lastName + "Would you like to search 'info', 'family',  'descendants', or 'next of kin'?, otherwise enter 'exit' or 'restart'.");
+        
+    switch (displayOption){
+
+        case "family":
+        getfamily(person, people);
+
+            break;
+           
+           case "info":
+            getinfo(person, people);
+
+            break;
+            
+            
 
             case "next of kin":
+            getnextOfKin(person, people);
 
-
-
-
-        break;
+            break;
 
 
             case "descendants":
-
-
-
-        break;
+            getDescendants(person, people);
+            break;
 
 
             case "restart":
+            initMostwanted(person, people);
             initMostwanted(people)
 
 
-        break;
+            break;
 
 
             case "quit":
+            
 
 
-         break;
+            return;
+            break;
+            
+
+        }
 
     }
-
+function getPerson(firstname, lastname, people){
+       var person = people.filter(person => person.firstName === firstname && person.lastName === lastname);
+       return person[0];
 }
-
-
-
-
-
-
-
-
-
-
-function getPerson(person, people) {
-     var listOfPeople = people.filter(person => person.firstname === fristname && person.lastname === lastname);
-     return listOfPeople;  
-
-}
-
-
-
-
-
-
